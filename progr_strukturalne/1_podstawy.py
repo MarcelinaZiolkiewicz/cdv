5ebf0448bbee2058128809d65a64f8b348f4fa3d
m.ziolkiewicz1@gmail.com <m.ziolkiewicz1@gmail.com>
2020-03-29 17:14:55 +0200

    File update

--- progr_strukturalne/1_podstawy.py
+++ progr_strukturalne/1_podstawy.py
@@ -1,2 +1,51 @@
 print("Hej")
 print(10+11)
+
+#pow=2**10
+#print(pow)
+#text = "Hejka"
+#print(text*2)
+
+#pobieranie danych z klawiatury
+# + to jest konkatenacja
+
+
+'''
+name = input("Podaj imię: ")
+surname = input("Podaj nazwisko: ")
+print("Twoje imie: " + name + ", nazwisko: " + surname);
+
+lengthName = str(len(name))
+lengthSurname = str(len(surname))
+
+print("Dlugosc imienia: " + lengthName)
+print("Dlugosc nazwiska: " + lengthSurname)
+
+print(type(lengthName))
+
+age = input("Podaj swoj wiek: ")
+name = input("Podaj imię: ")
+surname = input("Podaj nazwisko: ")
+
+# age=str(age)
+
+print("Imie i nazwisko:" + name + " " + surname + ", wiek: " + age)
+
+'''
+
+
+print("\nNarodowość: ", end="")
+nationality = input()
+
+surnamee = "Kowalski"
+firstLetter=surnamee[0]
+print(firstLetter)
+
+lastLetter = surnamee[len(surnamee)-1]
+print(lastLetter)
+
+
+a="10"
+print(type(a))
+a=int(a)
+print(type(a))
